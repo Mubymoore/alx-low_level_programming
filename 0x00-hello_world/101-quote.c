443836883d934ae8750227409d7d2f98c4938c1c
Mubymoore <omokayode2018@gmail.com>
2023-03-16 04:00:54 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - prints exactly and that piece of art is useful
+ * main - Entry point
  *
  * Return: Always 1 (Success)
  */
 int main(void)
-{
-	write(2,"and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
-	return (1);
-}
+{       
+        write(2,
+        "and that piece of art is useful\" - Dora korpar, 2015-10-19\n", 59);
+        return (1);
+}  
