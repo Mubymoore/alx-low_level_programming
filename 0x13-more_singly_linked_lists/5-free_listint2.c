65657e196675cd7a68ed495ecbecb718078b892f
Mubymoore <omokayode2018@gmail.com>
2023-05-01 01:27:11 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,14 +10,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-
-	if (head = NULL)
+	if (head == NULL)
 		return;
 
 	while (*head)
 	{
 		temp = (*head)->next;
-		free(*head);
+		free(head);
 		*head = temp;
 	}
 
