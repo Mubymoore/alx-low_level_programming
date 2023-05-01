806494541c87d1eff1709982121c16614241c019
Mubymoore <omokayode2018@gmail.com>
2023-05-01 01:41:45 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -14,7 +14,7 @@ listint_t *find_listint_loop(listint_t *head)
 	if (!head)
 		return (NULL);
 
-	while (slow & fast && fast->next)
+	while (slow && fast && fast->next)
 	{
 		fast = fast->next->next;
 		slow = slow->next;
@@ -27,6 +27,7 @@ listint_t *find_listint_loop(listint_t *head)
 				fast = fast->next;
 			}
 			return (fast);
+		}
 	}
 
 	return (NULL);
